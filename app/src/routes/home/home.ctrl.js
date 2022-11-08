cafb237700cef9e86c4d2b86e5010ac4d7e7f491
BSS <tjqjatjr1212@gamil.com>
2022-11-08 23:57:14 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,9 +9,29 @@ const output = {
     },
 }
 
+const users = {
+    id: ["민웅기", "서범석"],
+    psword: ["1234", "12345"],
+};
+
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+            psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            mag: "로그인에 실패하였습니다.",
+        })
     },
 };
 
